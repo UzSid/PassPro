5cb97ca5af5c0ac8826dcb8fcd49e340e4f4b659
YooZee <umasood000@gmail.com>
2024-11-26 14:24:21 -0500

    mostly finished functionality

--- project4/src/App.js
+++ project4/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(8);
+
   const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (event) => {
@@ -8,16 +10,16 @@ function App() {
   };  
 
   const handleClick = () => {
-    let fetchURL = 'https://api.genratr.com/?length=8';
-    if (document.getElementById("includeNumbers").checked == true) {fetchURL += "&numbers";}
-    if (document.getElementById("includeLower").checked == true) {fetchURL += "&lowercase";}
-    if (document.getElementById("includeUpper").checked == true) {fetchURL += "&uppercase";}
-    if (document.getElementById("includeSpecial").checked == true) {fetchURL += "&special";}
-    if (document.getElementById("includeNumbers").checked == false &&
-        document.getElementById("includeLower").checked == false &&
-        document.getElementById("includeUpper").checked == false &&
-        document.getElementById("includeSpecial").checked == false) {
-          alert("Choose");
+    let fetchURL = 'https://api.genratr.com/?length=' + document.getElementById("length").value;
+    if (document.getElementById("includeNumbers").checked === true) {fetchURL += "&numbers";}
+    if (document.getElementById("includeLower").checked === true) {fetchURL += "&lowercase";}
+    if (document.getElementById("includeUpper").checked === true) {fetchURL += "&uppercase";}
+    if (document.getElementById("includeSpecial").checked === true) {fetchURL += "&special";}
+    if (document.getElementById("includeNumbers").checked === false &&
+        document.getElementById("includeLower").checked === false &&
+        document.getElementById("includeUpper").checked === false &&
+        document.getElementById("includeSpecial").checked === false) {
+          alert("Check at least one box");
     }
     else {
       fetch(fetchURL)
@@ -33,36 +35,29 @@ function App() {
     <div className="App">
       <form action="http://localhost/projects/project4/project4/src/addpassword.php" method="GET">
         <label for="Website">Website:</label><br/>
-        <input type="text" name="Website"/><br/>
+        <input type="text" name="Website" required/><br/>
         <label for="Username">Username:</label><br/>
-        <input type="text" name="Username"/><br/>
+        <input type="text" name="Username" required/><br/>
         <label for="Password">Password:</label><br/>
-        <input type="text" name="Password" value={inputValue} onChange={handleInputChange}/><br/><br/>
+        <input type="text" name="Password" value={inputValue} onChange={handleInputChange} required/><br/><br/>
         <form>
-
-
-
-          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-          <p>Value: <span id="demo"></span></p>
-
-
-
-
-          <input type="checkbox" id="includeNumbers" name="includeNumbers" value="Bike"/>
+          <label for="length">Length</label><br/>
+          <input type="range" min="8" max="20" id="length" value={value} onChange={(e) => setValue(e.target.value)}/> {value} <br/>
+          <input type="checkbox" id="includeNumbers" name="includeNumbers" value="&numbers"/>
           <label for="includeNumbers">Include numbers</label><br/>
-          <input type="checkbox" id="includeLower" name="includeLower" value="Car"/>
+          <input type="checkbox" id="includeLower" name="includeLower" value="&lowercase"/>
           <label for="includeLower">Include lowercase letters</label><br/>
-          <input type="checkbox" id="includeUpper" name="includeUpper" value="Boat"/>
+          <input type="checkbox" id="includeUpper" name="includeUpper" value="&uppercase"/>
           <label for="includeUpper">Include uppercase letters</label><br/>
-          <input type="checkbox" id="includeSpecial" name="includeSpecial" value="Car"/>
+          <input type="checkbox" id="includeSpecial" name="includeSpecial" value="&special"/>
           <label for="includeSpecial">Include special characters</label><br/><br/>
           <button type="button" onClick={handleClick}>Generate Password</button>
         </form>
-        {inputValue.length < 8 ? (<p>Password must have at least 8 characters ❌</p>) : (<p>Password has at least 8 characters ✔</p>)}
-        {checkInt(inputValue) === false ? (<p>Password needs numbers ❌</p>) : <p>Password has numbers ✔</p>}
-        {checkLower(inputValue) === false ? (<p>Password needs lowercase letters ❌</p>) : (<p>Password has lowercase letters ✔</p>)}
-        {checkUpper(inputValue) === false ? (<p>Password needs uppercase letters ❌</p>) : (<p>Password has uppercase letters ✔</p>)}
-        {checkSpecial(inputValue) === false ? (<p>Password needs special characters ❌</p>) : (<p>Password has special characters ✔</p>)}
+        {inputValue.length < 8 ? (<p>Password should have at least 8 characters ❌</p>) : (<p>Password has at least 8 characters ✔</p>)}
+        {checkInt(inputValue) === false ? (<p>Password should have numbers ❌</p>) : <p>Password has numbers ✔</p>}
+        {checkLower(inputValue) === false ? (<p>Password should have lowercase letters ❌</p>) : (<p>Password has lowercase letters ✔</p>)}
+        {checkUpper(inputValue) === false ? (<p>Password should have uppercase letters ❌</p>) : (<p>Password has uppercase letters ✔</p>)}
+        {checkSpecial(inputValue) === false ? (<p>Password should have special characters ❌</p>) : (<p>Password has special characters ✔</p>)}
         <br/><br/><br/>
         <input type="submit"/>
       </form> 
--- project4/src/passwordList.js
+++ project4/src/passwordList.js
@@ -3,19 +3,34 @@ import React, { useState } from 'react';
 import {checkSpecial, checkLower, checkUpper, checkInt} from "./App";
 
 function PasswordList() {
+    const [value, setValue] = useState(8);
+    
     const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (event) => {
       setInputValue(event.target.value);
     };  
 
-    const handleClick = () => {
-        fetch('https://api.genratr.com/?length=8&uppercase&lowercase&special&numbers')
-        .then(response => response.json())
-        .then(json => {
-          let content = json.password;
-          setInputValue(content);
-        });    
+    function handleClick(id) {
+        let fetchURL = 'https://api.genratr.com/?length=' + document.getElementById("length").value;
+        if (document.getElementById("includeNumbers"+id).checked === true) {fetchURL += "&numbers";}
+        if (document.getElementById("includeLower"+id).checked === true) {fetchURL += "&lowercase";}
+        if (document.getElementById("includeUpper"+id).checked === true) {fetchURL += "&uppercase";}
+        if (document.getElementById("includeSpecial"+id).checked === true) {fetchURL += "&special";}
+        if (document.getElementById("includeNumbers"+id).checked === false &&
+            document.getElementById("includeLower"+id).checked === false &&
+            document.getElementById("includeUpper"+id).checked === false &&
+            document.getElementById("includeSpecial"+id).checked === false) {
+              alert("Check at least one box");
+        }
+        else {
+          fetch(fetchURL)
+          .then(response => response.json())
+          .then(json => {
+            let content = json.password;
+            setInputValue(content);
+          });
+        }   
     };
 
     fetch("http://localhost/projects/project4/project4/src/passwordList.php")
@@ -35,7 +50,7 @@ function PasswordList() {
                         <Collapsible trigger="Edit username">
                             <form action="http://localhost/projects/project4/project4/src/editUsername.php" method="GET">
                                 <label for="newUsername">New username:</label><br/>
-                                <input type="text" name="newUsername"/><br/>
+                                <input type="text" name="newUsername" required/><br/>
                                 <input type="hidden" name="ID" value={account.ID}/>
                                 <input type="submit"/>
                             </form>
@@ -44,13 +59,25 @@ function PasswordList() {
                         <Collapsible trigger="Edit password">
                             <form action="http://localhost/projects/project4/project4/src/editPassword.php" method="GET">
                                 <label for="newPassword">New password:</label><br/>
-                                <input type="text" name="newPassword" value={inputValue} onChange={handleInputChange}/><br/><br/>
-                                <button type="button" onClick={handleClick}>Generate Password</button>
-                                {inputValue.length < 8 ? (<p>Password must have at least 8 characters ❌</p>) : (<p>Password has at least 8 characters ✔</p>)}
-                                {checkInt(inputValue) === false ? (<p>Password needs numbers ❌</p>) : <p>Password has numbers ✔</p>}
-                                {checkLower(inputValue) === false ? (<p>Password needs lowercase letters ❌</p>) : (<p>Password has lowercase letters ✔</p>)}
-                                {checkUpper(inputValue) === false ? (<p>Password needs uppercase letters ❌</p>) : (<p>Password has uppercase letters ✔</p>)}
-                                {checkSpecial(inputValue) === false ? (<p>Password needs special characters ❌</p>) : (<p>Password has special characters ✔</p>)}
+                                <input type="text" name="newPassword" value={inputValue} onChange={handleInputChange} required/><br/><br/>
+                                <form>
+                                    <label for="length">Length</label><br/>
+                                    <input type="range" min="8" max="20" id="length" value={value} onChange={(e) => setValue(e.target.value)}/> {value} <br/>
+                                    <input type="checkbox" id={"includeNumbers"+account.ID} name="includeNumbers" value={account.ID}/>
+                                    <label for="includeNumbers">Include numbers</label><br/>
+                                    <input type="checkbox" id={"includeLower"+account.ID} name="includeLower" value={account.ID}/>
+                                    <label for="includeLower">Include lowercase letters</label><br/>
+                                    <input type="checkbox" id={"includeUpper"+account.ID} name="includeUpper" value={account.ID}/>
+                                    <label for="includeUpper">Include uppercase letters</label><br/>
+                                    <input type="checkbox" id={"includeSpecial"+account.ID} name="includeSpecial" value={account.ID}/>
+                                    <label for="includeSpecial">Include special characters</label><br/><br/>
+                                    <button type="button" onClick={() =>handleClick(account.ID)}>Generate Password</button>
+                                </form>
+                                {inputValue.length < 8 ? (<p>Password should have at least 8 characters ❌</p>) : (<p>Password has at least 8 characters ✔</p>)}
+                                {checkInt(inputValue) === false ? (<p>Password should have numbers ❌</p>) : <p>Password has numbers ✔</p>}
+                                {checkLower(inputValue) === false ? (<p>Password should have lowercase letters ❌</p>) : (<p>Password has lowercase letters ✔</p>)}
+                                {checkUpper(inputValue) === false ? (<p>Password should have uppercase letters ❌</p>) : (<p>Password has uppercase letters ✔</p>)}
+                                {checkSpecial(inputValue) === false ? (<p>Password should have special characters ❌</p>) : (<p>Password has special characters ✔</p>)}
                                 <input type="hidden" name="ID" value={account.ID}></input>
                                 <input type="submit"/>
                             </form>
